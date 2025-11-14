ff31ea578d43a8c634b9fd0dbc38ce54b3289116
rymilton <rmilton@ucla.edu>
2025-11-14 14:47:48 -0500

    Added all inputs to the saved output file

--- inference.py
+++ inference.py
@@ -33,7 +33,7 @@ def parse_arguments():
 
 def collate_fn(batch):
     # batch is a list of (hits, label, globals_event) tuples
-    hits, labels, globals_event, reco_pid, RICH_PID, RICH_RQ = zip(*batch)
+    hits, labels, globals_event, reco_pid, RICH_PID, RICH_RQ, rec_theta, rec_phi, cherenkov_angle = zip(*batch)
 
     # hits is a list of tensors with shape [num_hits, 3]
     lengths = [h.size(0) for h in hits]
@@ -53,14 +53,17 @@ def collate_fn(batch):
     reco_pid = torch.stack(reco_pid)
     RICH_PID = torch.stack(RICH_PID)
     RICH_RQ = torch.stack(RICH_RQ)
-    return hits_padded, labels, globals_event, mask, reco_pid, RICH_PID, RICH_RQ
+    rec_theta = torch.stack(rec_theta)
+    rec_phi = torch.stack(rec_phi)
+    cherenkov_angle = torch.stack(cherenkov_angle)
+
+    return hits_padded, labels, globals_event, mask, reco_pid, RICH_PID, RICH_RQ, rec_theta, rec_phi, cherenkov_angle
 
 def main():
     flags = parse_arguments()
     data_parameters = LoadYaml(flags.config, flags.config_directory)
     training_parameters = LoadYaml(flags.training_config, flags.config_directory)
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
-
     # --- Load test dataset ---
     data_path = os.path.join(
         data_parameters["SAVE_DIRECTORY"],
@@ -95,10 +98,14 @@ def main():
     all_reco_pid = []
     all_RICH_pid, all_RICH_RQ = [], []
     all_momentum = []
-
+    all_rec_theta, all_rec_phi = [], []
+    all_RICH_hits_x, all_RICH_hits_y, all_RICH_hits_time = [], [], []
+    all_mask = []
+    all_rec_traj_x, all_rec_traj_y, all_rec_traj_cx, all_rec_traj_cy, all_rec_traj_cz = [], [], [], [], []
+    all_cherenkov_angles = []
     print("Making predictions")
     with torch.no_grad():
-        for i, (hits_padded, labels, globals_event, mask, reco_pid, RICH_PID, RICH_RQ) in enumerate(dataloader):
+        for i, (hits_padded, labels, globals_event, mask, reco_pid, RICH_PID, RICH_RQ, rec_theta, rec_phi, cherenkov_angle) in enumerate(dataloader):
             hits_padded = hits_padded.to(device)
             globals_event = globals_event.to(device)
             mask = mask.to(device)
@@ -111,11 +118,44 @@ def main():
             momentum_unscaled = momentum*(12 - 1) + 1
             all_momentum.append(momentum_unscaled.cpu())
 
+            traj_x = globals_event[:, 1]
+            all_rec_traj_x.append(traj_x.cpu())
+
+            traj_y = globals_event[:, 2]
+            all_rec_traj_y.append(traj_y.cpu())
+
+            traj_cx = globals_event[:, 3]
+            all_rec_traj_cx.append(traj_cx.cpu())
+
+            traj_cy = globals_event[:, 4]
+            all_rec_traj_cy.append(traj_cy.cpu())
+
+            traj_cz = globals_event[:, 5]
+            all_rec_traj_cz.append(traj_cz.cpu())
+
             all_probabilities.append(probs.cpu())
             all_labels.append(labels)  # optional, if you want to compare
             all_reco_pid.append(reco_pid)
             all_RICH_pid.append(RICH_PID)
             all_RICH_RQ.append(RICH_RQ)
+            all_rec_theta.append(rec_theta)
+            all_rec_phi.append(rec_phi)
+            all_cherenkov_angles.append(cherenkov_angle)
+
+            rich_hit_time = hits_padded[:, :, 0]
+            rich_hit_time_unscaled = rich_hit_time*(19500 - 125) + (125)
+            all_RICH_hits_time.append(rich_hit_time_unscaled.cpu())
+
+            rich_hit_x = hits_padded[:, :, 1]
+            rich_hit_x_unscaled = rich_hit_x*(-37 - (-166)) + (-166)
+            all_RICH_hits_x.append(rich_hit_x_unscaled.cpu())
+
+            rich_hit_y = hits_padded[:, :, 2]
+            rich_hit_y_unscaled = rich_hit_y*(78 - (-81)) + (-81)
+            all_RICH_hits_y.append(rich_hit_y_unscaled.cpu())
+
+            all_mask.append(mask.cpu())
+
             
 
     print("Done with predictions. Saving.")
@@ -126,7 +166,14 @@ def main():
     all_momentum = torch.cat(all_momentum)
     all_RICH_pid = torch.cat(all_RICH_pid)
     all_RICH_RQ = torch.cat(all_RICH_RQ)
-
+    all_rec_theta = torch.cat(all_rec_theta)
+    all_rec_phi = torch.cat(all_rec_phi)
+    all_cherenkov_angles = torch.cat(all_cherenkov_angles)
+    all_rec_traj_x = torch.cat(all_rec_traj_x)
+    all_rec_traj_y = torch.cat(all_rec_traj_y)
+    all_rec_traj_cx = torch.cat(all_rec_traj_cx)
+    all_rec_traj_cy = torch.cat(all_rec_traj_cy)
+    all_rec_traj_cz = torch.cat(all_rec_traj_cz)
     # --- Save predictions ---
     output_file = os.path.join(training_parameters["MODEL_SAVE_DIRECTORY"], "test_predictions.pt")
     torch.save({
@@ -135,7 +182,19 @@ def main():
         "reco_pid": all_reco_pid,
         "RICH_PID": all_RICH_pid,
         "RICH_RQ": all_RICH_RQ,
-        "reco_momentum":all_momentum
+        "reco_momentum":all_momentum,
+        "reco_theta":all_rec_theta,
+        "reco_phi":all_rec_phi,
+        "reco_traj_x":all_rec_traj_x,
+        "reco_traj_y":all_rec_traj_y,
+        "reco_traj_cx":all_rec_traj_cx,
+        "reco_traj_cy":all_rec_traj_cy,
+        "reco_traj_cz":all_rec_traj_cz,
+        "RICH_hits_x":all_RICH_hits_x,
+        "RICH_hits_y":all_RICH_hits_y,
+        "RICH_hits_time":all_RICH_hits_time,
+        "RICH_hits_mask":all_mask,
+        "RICH_cherenkov_angle": all_cherenkov_angles,
     }, output_file)
 
     print(f"Predictions saved to {output_file}")
