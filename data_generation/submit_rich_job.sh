8be2d86e100115d8b6405678594eb3acaa53f5f8
rymilton <rmilton@ucla.edu>
2026-02-13 16:32:22 -0500

    Updated memory + time requests, using RGA configuration

--- data_generation/submit_rich_job.sh
+++ data_generation/submit_rich_job.sh
@@ -1,24 +1,28 @@
 #!/bin/bash
-#SBATCH --job-name=RICH_generation_0_499
+#SBATCH --job-name=RICH_generation_0_9999
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --time=5:00:00
+#SBATCH --partition=production
+#SBATCH --account=clas12
+#SBATCH --time=14:00:00
 #SBATCH --mem-per-cpu=4000
-#SBATCH --array=0-499
-#SBATCH --output=/volatile/clas12/rmilton/RICH_data_generation_12_2025/logs_3cmshift/slurm_%A_%a.out
-#SBATCH --error=/volatile/clas12/rmilton/RICH_data_generation_12_2025/logs_3cmshift/slurm_%A_%a.err
+#SBATCH --array=0-9999
+#SBATCH --output=/volatile/clas12/rmilton/RICH_data_generation_12_2025/logs_rgaspring2018/slurm_%A_%a.out
+#SBATCH --error=/volatile/clas12/rmilton/RICH_data_generation_12_2025/logs_rgaspring2018/slurm_%A_%a.err
 
 i=${SLURM_ARRAY_TASK_ID}
 
 source /etc/profile.d/modules.sh
 module use /scigroup/cvmfs/hallb/clas12/sw/modulefiles
 module load clas12/5.3
-module unload .scons_bm
+module unload gemc
 module load gemc/5.12
+module unload coatjava
+module load coatjava/11.1.0
 
-LUND_DIRECTORY=/volatile/clas12/rmilton/RICH_data_generation_12_2025/lund_3cmshift/
-HIPO_DIRECTORY=/volatile/clas12/rmilton/RICH_data_generation_12_2025/hipo_3cmshift/
-RECO_DIRECTORY=/volatile/clas12/rmilton/RICH_data_generation_12_2025/reco_3cmshift/
+LUND_DIRECTORY=/volatile/clas12/rmilton/RICH_data_generation_12_2025/lund_rgaspring2018/
+HIPO_DIRECTORY=/volatile/clas12/rmilton/RICH_data_generation_12_2025/hipo_rgaspring2018/
+RECO_DIRECTORY=/volatile/clas12/rmilton/RICH_data_generation_12_2025/reco_rgaspring2018/
 
 printf -v ii "%04d" "$i"
 lund_file=${LUND_DIRECTORY}/file_${ii}.dat
@@ -31,12 +35,12 @@ mkdir -p $RECO_DIRECTORY
 
 $GEMC/bin/gemc \
 -USE_GUI=0 \
--N=1000 \
-/volatile/clas12/rmilton/RICH_data_generation_12_2025/rgc_summer2022.gcard \
+-N=10000 \
+/volatile/clas12/rmilton/RICH_data_generation_12_2025/rga_spring2018.gcard \
 -INPUT_GEN_FILE="LUND, ${lund_file}" \
 -OUTPUT="hipo, ${sim_file}"
 
 recon-util -y \
-/volatile/clas12/rmilton/RICH_data_generation_12_2025/rgc_summer2022.yaml \
+/volatile/clas12/rmilton/RICH_data_generation_12_2025/rga_spring2018.yaml \
 -i ${sim_file} \
 -o ${reco_file}
