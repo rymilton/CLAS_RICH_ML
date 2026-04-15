12e8d33d2c1fc2bead0a02b608043bd2be8837d7
rymilton <rmilton@ucla.edu>
2026-04-15 19:01:41 -0400

    Updated data paths. Included not overwriting existing files

--- hipo2root.sh
+++ hipo2root.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
-data_directory="/work/clas12/pecar/RICH/gemcSim/clas12-rich-sim/clas12Tags-5.10/recoFiles/DIS/default/allOpticsNominal/"
-output_directory="/volatile/clas12/rmilton/RICH_data/"
+data_directory="/home/rmilton/work_dir/RICH_data_generation_12_2025/reco_rgaspring2018_hipo/"
+output_directory="/home/rmilton/work_dir/RICH_data_generation_12_2025/reco_rgaspring2018_root/"
+mkdir -p "${output_directory}"
 
-mkdir -p ${output_directory}
-
+count=0
 for file in "${data_directory}"/*; do
     echo "Converting ${file}"
-    filename=$(basename "$file") 
-    ./RICH_hipo2root ${data_directory} ${filename} ${output_directory}
+    filename=$(basename "$file")
+    output_file="${output_directory}/${filename}.root"
+    if [[ -f "$output_file" ]]; then
+        echo "Skipping ${filename}: ${output_file} already exists."
+        continue
+    fi
+    ./RICH_hipo2root "${data_directory}" "${filename}" "${output_directory}"
+
+    ((count++))
 done
